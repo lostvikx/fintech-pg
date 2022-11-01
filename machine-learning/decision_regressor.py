760b26ac0f524e1b0f9e4e2ab75b0d6a9d5b20c9
Vikram Negi <viknegi0@gmail.com>
2022-11-01 12:43:22 +0530

    decision regressor & classifier

---
+++ machine-learning/decision_regressor.py
@@ -0,0 +1,27 @@
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeRegressor
+
+# %%
+d = pd.read_csv("datasets/Position_Salaries.csv")
+d.head()
+
+# %%
+x = d.iloc[:,1:2].values
+y = d.iloc[:,2].values
+
+# %%
+regressor = DecisionTreeRegressor()
+regressor.fit(x,y)
+
+# %%
+y_pred = regressor.predict([[2.5]])
+y_pred
+
+#%%
+x_grid = np.arange(min(x),max(x),0.01)
+# x_grid = x_grid.reshape((len(x_grid),1))
+# plt.scatter(x,y,color="red")
+# plt.plot(x,regressor.predict(x_grid))
--- machine-learning/decision_tree.py
+++ machine-learning/decision_tree.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 # %%
@@ -26,26 +26,13 @@ sc = StandardScaler()
 x_train = sc.fit_transform(x_train)
 x_test = sc.transform(x_test)
 
-# %%
-# Fitting Logistic Regression to training set
-classifier = LogisticRegression(random_state=0)
-classifier.fit(x_train,np.ravel(y_train))
+#%%
+# Can eb entropy or gini
+classifier = DecisionTreeClassifier(criterion="entropy",random_state=0)
+classifier.fit(x_train,y_train)
 
 # %%
-# Predict the test set
 y_pred = classifier.predict(x_test)
 
 # %%
-# Test the accuracy of the model
-accuracy_score(y_test,y_pred)
-
-# %%
-prob = classifier.predict_proba(x_train)
-
-# %%
-prob = pd.DataFrame(prob)
-prob.head()
-
-# %%
-# Confusion Matrix
 confusion_matrix(y_test,y_pred)
