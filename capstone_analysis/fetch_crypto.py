3c69764aa5cb479ae671f46afc4eccf4d4f20655
Vikram Negi <viknegi0@gmail.com>
2023-01-20 13:22:09 +0530

    fetch crypto ohlc script

--- capstone_analysis/fetch_crypto.py
+++ capstone_analysis/fetch_crypto.py
@@ -1,19 +1,25 @@
 import requests
 import pandas as pd
+import os
 
-def get_crypto_historic_prices(symbol, exchange="bitstamp", after="2022-08-01"):
+def get_crypto_historic_prices(symbol, exchange="bitstamp", after="2018-01-01", period="86400"):
   url = f"https://api.cryptowat.ch/markets/{exchange}/{symbol}usd/ohlc"
 
   res = requests.get(url, params={
-    "period": "3600",
+    "period": period,
     "after": str(int(pd.Timestamp(after).timestamp()))
   })
 
   res.raise_for_status()
   data = res.json()
   
+  # tokens
+  cost = data.get("allowance").get("cost")
+  remaining = data.get("allowance").get("remaining")
+  print(f"Cost: {cost}\nRemaining: {remaining}")  # Cost: 0.015
+
   # 3600s (fetch hourly data)
-  df = pd.DataFrame(data["result"]["3600"], columns=[
+  df = pd.DataFrame(data["result"][period], columns=[
     "close_time", "open", "high", "low", "close", "volume", "quote_volume"
   ])
 
@@ -24,10 +30,14 @@ def get_crypto_historic_prices(symbol, exchange="bitstamp", after="2022-08-01"):
   return df
 
 def main():
+  dir_path = os.path.dirname(os.path.realpath(__file__))
+
   # btc and eth are the two most popular/traded crypto-currencies
-  btc = get_crypto_historic_prices("btc")
-  print(btc.tail())
-  print(btc.shape)
+  currencies = ["btc","eth"]
+  for currency in currencies:
+    df = get_crypto_historic_prices(currency)
+    print(df.shape)
+    df.to_csv(f"{dir_path}/data/{currency}_ohlc_5years.csv")
 
 if __name__ == "__main__":
   main()
