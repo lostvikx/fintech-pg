41d077ac8c5ce4f812ee26d82385f88da4ffa824
Vikram Negi <viknegi0@gmail.com>
2023-01-20 16:41:35 +0530

    one epoch only

--- capstone_analysis/crypto_technical.py
+++ capstone_analysis/crypto_technical.py
@@ -139,6 +139,13 @@ for label, x, y in zip(["BTC","ETH"],rets.mean(),rets.std()):
   plt.annotate(label,xy=(x,y),xytext=(30,30),textcoords="offset points",ha="right",va="bottom",arrowprops=dict(arrowstyle="-",color="black",connectionstyle="arc3,rad=-0.2"))
 
 plt.show()
+# %% [markdown]
+# This shows that ETH is a high risk or more volatile cryptocurrency than BTC, but also provides higher returns. Higher the risk, higher the expected return.
+#
+# ## Deep Learning for Crypto price prediction
+# There are several machine learning models that can be used to predict stock prices. One such model is Long Short-Term Memory (LSTM). 
+# 
+# The LSTM (Long Short-Term Memory) layers are a type of recurrent neural network (RNN) that are commonly used for time series data, like stock prices.
 # %%
 btc_df = pd.read_csv("data/btc_ohlc_5years.csv",index_col="close_time",parse_dates=True)
 btc_df.head()
@@ -158,8 +165,10 @@ print(f"Data shape: {data.shape}")
 # 80% train & 20% test dataset
 train_data_len = int(len(data) * 0.80)
 print(f"Train data len: {train_data_len}")
+# %% [markdown]
+# Transform features by scaling each feature to a given range. Commonly the feature range is from 0 to 1. This helps optimize the data, for faster algorithm runtime.
+
 # %%
-# Transform features by scaling each feature to a given range
 from sklearn.preprocessing import MinMaxScaler
 
 scaler = MinMaxScaler(feature_range=(0,1))
@@ -182,6 +191,11 @@ x_train,y_train = np.array(x_train),np.array(y_train)
 # Convert to a 3D-array
 x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
 x_train.shape
+# %% [markdown]
+# From the set of 50-days close price of the currency, we want to predict the 51th day close price.
+# 
+# * Predictor variable: 50-day close price
+# * Target variable: 51th close price
 # %%
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
@@ -195,9 +209,33 @@ model.add(Dense(1))
 
 # Compile the model
 model.compile(optimizer="adam",loss="mean_squared_error")
+# %% [markdown]
+# The first LSTM layer has 128 units and the input shape is taken from `x_train.shape[1]` and it has return_sequences=True which is used to connect with next LSTM layer.
+#
+# The second LSTM layer has 64 units and return_sequences is set to False which means it is the last layer of the LSTM network.
+#
+# The first dense layer has 25 units and the last dense layer has 1 unit. This architecture is used for regression problem.
+#
+# The model is then compiled with the Adam optimizer and mean squared error (MSE) loss function. This means that the model will be trained to minimize the MSE between the predicted values and the true values in the training data.
 # %%
-# TODO: Change epochs to 10
-model.fit(x_train,y_train,epochs=2,batch_size=1)
+model.fit(x_train,y_train,epochs=1,batch_size=1)
+# %% [markdown]
+# Few general guidelines for LSTM parameters:
+# 
+# LSTM:
+# * The number of units in the LSTM layer can affect the model's ability to capture long-term dependencies in the data. A larger number of units generally means that the model has more capacity to learn complex patterns, but it also increases the risk of overfitting. A good starting point is typically in the range of 64 to 256 units.
+#
+# Dense layers:
+# * The number of units in the last dense layer should be equal to the number of output classes.
+#
+# batch_size: 
+# * The batch size used during training. This is the number of samples per gradient update. 
+# * The larger the batch size, the more memory space you'll need. But smaller batch size can lead to more updates and that can result in a better model.
+#
+# epochs:
+# * The number of times the model will cycle through the data. One epoch is when an entire dataset is passed forward and backward through the neural network only once.
+# * Typically, a larger number of epochs is used to train the model, like 10 or 20, to allow the model to learn from more examples.
+# * In our case, we use `epoch = 1` for as this can be computationally expensive.
 # %%
 test_data = scaled_data[train_data_len-50:]
 x_test = []
@@ -205,7 +243,6 @@ y_test = data[train_data_len:,:]
 
 for i in range(50,len(test_data)):
   x_test.append(test_data[i-50:i,0])
-  # y_test.append(test_data[i,0])
 
 x_test,y_test = np.array(x_test),np.array(y_test)
 x_test = np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
@@ -237,3 +274,6 @@ plt.ylabel("Close Price ($)")
 
 plt.legend(["Train","Validate","Predicted"])
 plt.show()
+# %% [markdown]
+# ### Why LSTM?
+# Recurrent Neural Networks (RNNs) are a type of neural network that allows for information to be passed from one step of the network to the next. LSTM (Long Short-Term Memory) is a specific type of RNN that is able to effectively learn and retain long-term dependencies by using gates to control the flow of information. These gates can be thought of as switches that determine whether to let new information in, or to keep previous information stored. This allows LSTMs to better handle tasks that require remembering previous events, such as language translation, speech recognition, stock price prediction.
